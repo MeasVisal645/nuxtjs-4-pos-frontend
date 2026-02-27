8596f7441e99aa4c9d7bc129757923d97477ef6e
LinKaiz645 <measvathanakvisal@gmail.com>
2026-02-27 11:47:06 +0700

    change to subtotal instead of total

--- app/pages/receipt.vue
+++ app/pages/receipt.vue
@@ -48,7 +48,6 @@ const print = () => {
       </div>
 
       <div>
-        <!-- <p class="text-black">Order No: {{ order.orderNo }}</p> -->
         <p class="text-black">Customer: {{ order.customerName }}</p>
         <p class="text-black">Payment Method: {{ order.paymentMethod }}</p>
         <p class="text-black">Sale By: {{ order.username }}</p>
@@ -56,25 +55,21 @@ const print = () => {
 
       <div class="py-2 space-y-1">
         <div class="flex justify-between font-bold text-lg pb-1">
-          <span class="text-black">Name</span>
           <span class="text-black">Quantity</span>
+          <span class="text-black">Name</span>
           <span class="text-black">Price</span>
         </div>
 
         <div v-for="item in order.items" :key="item.name" class="flex justify-between border-t border-dashed py-2 border-gray-400">
-          <span class="text-black">{{ item.name }} {{ item.quantity }} </span>
+          <span class="text-black">{{ item.quantity }}x {{ item.name }}</span>
           <span class="text-black">{{ (item.price * item.quantity) }}៛</span>
         </div>
       </div>
 
       <div class=" pt-2 mt-2 space-y-1">
-        <!-- <div class="flex justify-between font-bold text-lg">
-          <span class="text-black">Tax(10%)</span>
-          <span class="text-black">{{ KHR(order.taxKHR) }} <span class="text-black text-xs">({{ USD(order.taxUSD) }})</span></span>
-        </div> -->
         <div class="flex justify-between font-bold text-md border-t border-gray-400 pt-2">
           <span class="text-black">TOTAL</span>
-          <span class="text-black">{{ KHR(order.totalKHR) }} <span class="text-black text-xs">({{ USD(order.totalUSD) }})</span></span>
+          <span class="text-black">{{ KHR(order.subtotalKHR) }} <span class="text-black text-xs">({{ USD(order.subtotalUSD) }})</span></span>
         </div>
       </div>
 
@@ -94,7 +89,6 @@ const print = () => {
 </template>
 
 <style scoped>
-/* Ensure only the receipt prints, no extra browser margins */
 @media print {
   @page {
     margin: 0;
